841194a6f19ec8080ebc96352bf4bfc46f5398a7
telsa <telsaiori.git@gmail.com>
2017-04-24 14:54:24 +0800

    add devise email related function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,:confirmable,
          :recoverable, :rememberable, :validatable,:omniauthable, omniauth_providers: [:facebook]
   has_and_belongs_to_many :roles
   
