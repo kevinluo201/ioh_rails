c51b021b2919081d19659ab573c5432fe7b98b46
telsa <telsaiori.git@gmail.com>
2017-04-24 11:51:36 +0800

    add role model and set seed.rb to create user roles

--- app/controllers/static_page_controller.rb
+++
@@ -1,4 +0,0 @@
-class StaticPageController < ApplicationController
-  def index
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'static_page#index'
+  root 'static_pages#index'
 end
